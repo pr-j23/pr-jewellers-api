7731e4b67103802e2708f82e5c6df929192cc739
pr-j23 <pavanraj577@gmail.com>
2024-12-24 21:54:54 +0530

    removed cors origin

--- src/middleware/setup.js
+++ src/middleware/setup.js
@@ -1,10 +1,9 @@
 import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
 import { errorHandler } from './error-handler';
-import { config } from '../config/app-config';
 
 export const setupMiddleware = (app) => {
-  app.use('*', logger());
-  app.use('*', cors({ origin: config.corsOrigins }));
-  app.use('*', errorHandler());
+	app.use('*', logger());
+	app.use('*', cors());
+	app.use('*', errorHandler());
 };
